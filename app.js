7ad65de09e634938ecd5915a3a59d66e5819ecf8
Kexin <kexinhao01111993@gmail.com>
2019-11-04 16:30:19 +0100

    4.nov. corrected

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const fs = require("fs");
 var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
-
-var port = 8081;
-var app = express();
+//
+// var port = 8081;
+// var app = express();
 
 //code here is from https://www.penta-code.com/scraping-youtube-comments-with-puppeteer/
 //I am hoping this part could help 1.navigate to the taobao site, 2.check if the page is loaded, 3.to scroll down the lazy loaded part and find the img.
@@ -31,24 +31,29 @@ async function getElText(page, selector) {
   await page.waitFor(2000);
   await page.waitForSelector('#img');
   await navigationPromise;
+	const imageUrl = await page.evaluate(() =>
+	    document.querySelector("img") // image selector
+	    );
+			console.log(imageUrl);
   // await page.waitForSelector('.style-scope:nth-child(1) > #comment > #body > #main > #header > #header-author > #author-text > .style-scope')
   //
   //
-  // const comments = [];
+  // const images = [];
   // for (let i = 1; i < 5; i++) {
-  //    const authorSelector = `.style-scope:nth-child(${i}) > #comment > #body > #main > #header > #header-author > #author-text > .style-scope`
-  //    const commentSelector = `.style-scope:nth-child(${i}) > #comment > #body > #main > #expander #content-text`;
-  //    await page.waitForSelector(commentSelector);
-  //    await page.waitForSelector(authorSelector);
-  //    const commentText = await getElText(page, commentSelector);
-  //    const author = await getElText(page, authorSelector);
-  //
-  //    if (commentText) {
+  //    // const authorSelector = `.style-scope:nth-child(${i}) > #comment > #body > #main > #header > #header-author > #author-text > .style-scope`
+  //    // const commentSelector = `.style-scope:nth-child(${i}) > #comment > #body > #main > #expander #content-text`;
+  //    await page.waitForSelector("img");
+	// 	 const innerHTML = await page.evaluate(() => document.querySelector('img').attr("src"));
+  //    // await page.waitForSelector(authorSelector);
+  //    // const commentText = await getElText(page, commentSelector);
+  //    // const author = await getElText(page, authorSelector);
+	//
+  //    // if (commentText) {
   //   // write each comment to DB or file
   //   // or batch the for processing later
-  //   console.log(`${author}: ${commentText}`);
+  //   console.log(innerHTML);
   //   comments.push(commentText);
-  //     }
+  //     // }
   //   }
 
 
@@ -83,34 +88,34 @@ async function getElText(page, selector) {
 
 
 // code here is from the workshop we had in the class. I do not know how it connects properly and reasonablly with the code above.
-app.get('/taobao', function (req, res) {
-    var url = "https://item.taobao.com/item.htm?spm=a230r.1.14.265.365915d6JmYCed&id=551660449648&ns=1&abbucket=11#detail";
-    request(url, function (error, response, html) {
-        if (!error) {
-
-            var imagesList = [];
-
-            var $ = cheerio.load(html);
-
-            $('img').each(function(){
-              imagesList.push($(this).attr('src'));
-            });
-
-            res.send(html);
-            fs.writeFile('taobao_output.js', "var taobao_output = [" + imagesList + "]", function(error){
-            console.log("File is written sucessfully!");
-            });
-        }
-    });
-});
+// app.get('/taobao', function (req, res) {
+//     var url = "https://item.taobao.com/item.htm?spm=a230r.1.14.265.365915d6JmYCed&id=551660449648&ns=1&abbucket=11#detail";
+//     request(url, function (error, response, html) {
+//         if (!error) {
+//
+//             var imagesList = [];
+//
+//             var $ = cheerio.load(html);
+//
+//             $('img').each(function(){
+//               imagesList.push($(this).attr('src'));
+//             });
+//
+//             res.send(html);
+//             fs.writeFile('taobao_output.js', "var taobao_output = [" + imagesList + "]", function(error){
+//             console.log("File is written sucessfully!");
+//             });
+//         }
+//     });
+// });
 
 
 //this part is from https://www.penta-code.com/scraping-youtube-comments-with-puppeteer/
  await browser.close();
 })()
 
-scrape()
+// scrape()
 
-app.listen(port);
-console.log('Magic happens on port' + port);
-exports = module.exports = app;
+// app.listen(port);
+// console.log('Magic happens on port' + port);
+// exports = module.exports = app;
